a2234a3cb7c07de44c72a5ccb79e0da488fbee59
pawarherschel <pawarherschel@gmail.com>
2024-01-11 20:27:43 +0530

    1026

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 mod zaphkiel;
 
+use cargo_leet::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+
 pub fn contains_duplicate_217(nums: Vec<i32>) -> bool {
     use std::collections::HashSet;
 
@@ -168,15 +172,14 @@ pub fn wheresmyinternet(input: String) -> String {
         max: usize,
         flags: &mut Vec<bool>,
     ) {
-        if max <= 0 || flags[ele - 1] {
-            return;
-        }
-
         flags[ele - 1] = true;
         let binding = HashSet::new();
         let cons = map.get(&ele).unwrap_or(&binding); // Use unwrap_or to handle None
 
         for &neighbor in cons {
+            if flags[neighbor - 1] {
+                dbg!(neighbor, cons);
+            }
             if !flags[neighbor - 1] {
                 descent(map, neighbor, max - 1, flags);
             }
@@ -204,6 +207,151 @@ pub fn wheresmyinternet(input: String) -> String {
     }
 }
 
+// pub fn amount_of_time_2385(args: (Option<Rc<RefCell<TreeNode>>>, i32)) -> i32 {
+//     let (root, start) = args;
+//     // this works only when the starting value is on the shorter branch
+//     pub fn amount_of_time(root: Option<Rc<RefCell<TreeNode>>>, start: i32) -> i32 {
+//         let left_sub_tree = root.clone().unwrap().borrow().left.clone();
+//         let right_sub_tree = root.clone().unwrap().borrow().right.clone();
+
+//         pub fn find_depth(
+//             node: Option<Rc<RefCell<TreeNode>>>,
+//             val: i32,
+//             curr: usize,
+//         ) -> Option<usize> {
+//             if let Some(node) = node {
+//                 if node.borrow().val == val {
+//                     // found the starting node
+//                     Some(curr)
+//                 } else {
+//                     let left_node = node.borrow().left.clone();
+//                     let right_node = node.borrow().right.clone();
+
+//                     if let Some(found) = find_depth(left_node, val, curr + 1) {
+//                         // found it in the left sub-tree
+//                         Some(found)
+//                     } else if let Some(found) = find_depth(right_node, val, curr + 1) {
+//                         // found it in the right sub-tree
+//                         Some(found)
+//                     } else {
+//                         // not present in the current sub-tree
+//                         None
+//                     }
+//                 }
+//             } else {
+//                 // went past the leaf node
+//                 None
+//             }
+//         }
+
+//         let find_in_left = find_depth(left_sub_tree.clone(), start, 0);
+//         let find_in_right = find_depth(right_sub_tree.clone(), start, 0);
+
+//         pub fn find_deepest(
+//             node: Option<Rc<RefCell<TreeNode>>>,
+//             highest: usize,
+//             curr: usize,
+//         ) -> usize {
+//             if let Some(node) = node {
+//                 let left_node = node.borrow().left.clone();
+//                 let right_node = node.borrow().right.clone();
+
+//                 let left_depth = find_deepest(left_node, highest, curr + 1);
+//                 let right_depth = find_deepest(right_node, highest, curr + 1);
+
+//                 let deeper = if left_depth > right_depth {
+//                     left_depth
+//                 } else {
+//                     right_depth
+//                 };
+
+//                 if highest < deeper {
+//                     deeper
+//                 } else {
+//                     highest
+//                 }
+//             } else {
+//                 // went past the leaf node
+//                 if highest < curr {
+//                     // found a new record depth
+//                     curr
+//                 } else {
+//                     // found a smaller depth
+//                     highest
+//                 }
+//             }
+//         }
+
+//         let deepest_left = find_deepest(left_sub_tree, 0, 0);
+//         let deepest_right = find_deepest(right_sub_tree, 0, 0);
+
+//         let deepest = if deepest_left > deepest_right {
+//             deepest_left
+//         } else {
+//             deepest_right
+//         };
+
+//         match (find_in_left, find_in_right, deepest_left > deepest_right) {
+//             (Some(depth), None, true) => {
+//                 // found in left + left is deeper
+//                 let from_node_to_deepest = TryInto::<i32>::try_into(deepest - depth).unwrap() - 1;
+
+//                 let from_node_to_
+
+//             }
+//             (None, Some(depth), false) => {
+//                 // found in right + right is deeper
+//                 let node_to_deepest = TryInto::<i32>::try_into(deepest - depth).unwrap() - 1;
+
+//                 depth.try_into().unwrap()
+//             }
+//             (Some(depth), None, false) => {
+//                 // found in left + right is deeper
+
+//                 TryInto::<i32>::try_into(deepest + depth).unwrap() + 1
+//             }
+//             (None, Some(depth), true) => {
+//                 // found in right + left is deeper
+//                 TryInto::<i32>::try_into(deepest + depth).unwrap() + 1
+//             }
+//             (None, None, _) => {
+//                 // root node was the starting point
+//                 deepest.try_into().unwrap()
+//             }
+//             huh => {
+//                 panic!("this should not be possible: {huh:?}")
+//             }
+//         }
+//     }
+//     amount_of_time(root, start)
+// }
+
+pub fn max_ancestor_diff_1026(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    pub fn recurse(node: Option<Rc<RefCell<TreeNode>>>, min_so_far: i32, max_so_far: i32) -> i32 {
+        if let Some(node) = node {
+            let val = node.borrow().val;
+            let left = node.borrow().left.clone();
+            let right = node.borrow().right.clone();
+
+            let min = val.min(min_so_far);
+            let max = val.max(max_so_far);
+
+            let left_result = recurse(left, min, max);
+            let right_result = recurse(right, min, max);
+
+            left_result.max(right_result)
+        } else {
+            max_so_far - min_so_far
+        }
+    }
+
+    recurse(
+        root.clone(),
+        root.clone().unwrap().borrow().val,
+        root.clone().unwrap().borrow().val,
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -215,52 +363,61 @@ mod tests {
         pub static ref MAP: RwLock<HashMap<String, Duration>> = RwLock::new(HashMap::new());
     }
 
-    generate_tests![MAP;
-        test_217_contains_duplicate_1, contains_duplicate_217, vec![1,2,3,1], true;
-        test_217_contains_duplicate_2, contains_duplicate_217, vec![1,2,3,4], false;
-        test_217_contains_duplicate_3, contains_duplicate_217, vec![1,1,1,3,3,4,3,2,4,2], true;
+    generate_tests! {MAP;
+    test_217_contains_duplicate_1, contains_duplicate_217, vec![1,2,3,1], true;
+    test_217_contains_duplicate_2, contains_duplicate_217, vec![1,2,3,4], false;
+    test_217_contains_duplicate_3, contains_duplicate_217, vec![1,1,1,3,3,4,3,2,4,2], true;
 
-        test_242_valid_anagram_1, valid_anagram_242, ("anagram".into(), "nagaram".into()), true;
-        test_242_valid_anagram_2, valid_anagram_242, ("rat".into(), "car".into()), false;
+    test_242_valid_anagram_1, valid_anagram_242, ("anagram".into(), "nagaram".into()), true;
+    test_242_valid_anagram_2, valid_anagram_242, ("rat".into(), "car".into()), false;
 
-        test_1_two_sum_1, two_sum_1, (vec![2,7,11,15], 9), vec![0, 1];
-        test_1_two_sum_2, two_sum_1, (vec![3,2,4], 6), vec![1,2];
-        test_1_two_sum_3, two_sum_1, (vec![3,3], 6), vec![0,1];
+    test_1_two_sum_1, two_sum_1, (vec![2,7,11,15], 9), vec![0, 1];
+    test_1_two_sum_2, two_sum_1, (vec![3,2,4], 6), vec![1,2];
+    test_1_two_sum_3, two_sum_1, (vec![3,3], 6), vec![0,1];
 
-        /* test_49_group_anagrams_1, */
+    /* test_49_group_anagrams_1, */
 
-        test_hr_simple_array_sum_1, hr_simple_array_sum, &[1, 2, 3, 4, 10, 11], 31;
-        test_hr_compare_the_triplets, hr_compare_the_triplets, (&[5, 6, 7], &[3, 6, 10]), vec![1, 1];
-        test_hr_a_very_big_sum, a_very_big_sum, &[1000000001, 1000000002, 1000000003, 1000000004, 1000000005], 5000000015;
-        test_hr_diagonal_difference, diagonal_difference, &[vec![11, 2, 4], vec![4, 5, 6], vec![10, 8, -12]], 15;
-        test_hr_plus_minus, plus_minus, &[-4, 3, -9, 0, 4, 1], true;
+    test_hr_simple_array_sum_1, hr_simple_array_sum, &[1, 2, 3, 4, 10, 11], 31;
+    test_hr_compare_the_triplets, hr_compare_the_triplets, (&[5, 6, 7], &[3, 6, 10]), vec![1, 1];
+    test_hr_a_very_big_sum, a_very_big_sum, &[1000000001, 1000000002, 1000000003, 1000000004, 1000000005], 5000000015;
+    test_hr_diagonal_difference, diagonal_difference, &[vec![11, 2, 4], vec![4, 5, 6], vec![10, 8, -12]], 15;
+    test_hr_plus_minus, plus_minus, &[-4, 3, -9, 0, 4, 1], true;
 
-        test_kattis_wheresmyinternet_1, wheresmyinternet, "2 1\n2 1".to_string(), "Connected".to_string();
-        test_kattis_wheresmyinternet_2, wheresmyinternet, "6 4\n1 2\n2 3\n3 4\n5 6".to_string(), "5\n6".to_string();
-        test_kattis_wheresmyinternet_3, wheresmyinternet, "4 3\n2 3\n4 2\n3 4".to_string(), "2\n3\n4".to_string();
-    ];
+    test_kattis_wheresmyinternet_1, wheresmyinternet, "2 1\n2 1".to_string(), "Connected".to_string();
+    test_kattis_wheresmyinternet_2, wheresmyinternet, "6 4\n1 2\n2 3\n3 4\n5 6".to_string(), "5\n6".to_string();
+    test_kattis_wheresmyinternet_3, wheresmyinternet, "4 3\n2 3\n4 2\n3 4".to_string(), "2\n3\n4".to_string();
 
-    //    #[test]
-    //    pub fn print_map() {
-    //        let no_of_tests = vec![3, 2, 3].iter().sum();
-    //        let mut map: HashMap<String, Duration> = MAP.read().unwrap().clone();
-    //
-    //        loop {
-    //            if map.len() != no_of_tests {
-    //                dbg!(map.len() != no_of_tests, map.len(), no_of_tests);
-    //                std::thread::sleep(Duration::from_secs(1));
-    //            } else {
-    //                break;
-    //            }
-    //            map = MAP.read().unwrap().clone();
-    //        }
-    //        let mut map = map.iter().collect::<Vec<(&String, &Duration)>>();
-    //        map.sort_by(|(k1, _), (k2, _)| k1.cmp(k2));
-    //        let map = map
-    //            .iter()
-    //            .map(|(k, v)| format!("{k} => {v:#?}"))
-    //            .collect::<Vec<String>>();
-    //        println!("\n\n\n\n\n");
-    //        dbg!(map);
-    //    }
+    // test_2385_amount_of_time_for_binary_tree_to_be_infected_1, amount_of_time_2385, (TreeRoot::from("[1,5,3,null,4,10,6,9,2]").into(), 3), 4;
+    // test_2385_amount_of_time_for_binary_tree_to_be_infected_2, amount_of_time_2385, (TreeRoot::from("[1,2,null,3,null,4,null,5]").into(), 2), 3;
+    // test_2385_amount_of_time_for_binary_tree_to_be_infected_3, amount_of_time_2385, (TreeRoot::from("[1]").into(), 0), 0;
+    // test_2385_amount_of_time_for_binary_tree_to_be_infected_4, amount_of_time_2385, (TreeRoot::from("[1,2,null,3,null,4,null,5]").into(), 4), 3;
+    // test_2385_amount_of_time_for_binary_tree_to_be_infected_5, amount_of_time_2385, (TreeRoot::from("[5,2,3,4,null,null,null,1]").into(), 4), 3;
+    // test_2385_amount_of_time_for_binary_tree_to_be_infected_6, amount_of_time_2385, (TreeRoot::from("[1,null,2,3,4,null,5]").into(), 4), 3;
+
+    test_1026_maximum_difference_between_node_and_ancestor_1, max_ancestor_diff_1026, TreeRoot::from("[8,3,10,1,6,null,14,null,null,4,7,13]").into(), 7;
+        }
+
+    // #[test]
+    // pub fn print_map() {
+    //     let no_of_tests = vec![3, 2, 3].iter().sum();
+    //     let mut map: HashMap<String, Duration> = MAP.read().unwrap().clone();
+
+    //     loop {
+    //         if map.len() != no_of_tests {
+    //             dbg!(map.len() != no_of_tests, map.len(), no_of_tests);
+    //             std::thread::sleep(Duration::from_secs(1));
+    //         } else {
+    //             break;
+    //         }
+    //         map = MAP.read().unwrap().clone();
+    //     }
+    //     let mut map = map.iter().collect::<Vec<(&String, &Duration)>>();
+    //     map.sort_by(|(k1, _), (k2, _)| k1.cmp(k2));
+    //     let map = map
+    //         .iter()
+    //         .map(|(k, v)| format!("{k} => {v:#?}"))
+    //         .collect::<Vec<String>>();
+    //     println!("\n\n\n\n\n");
+    //     dbg!(map);
+    // }
 }
