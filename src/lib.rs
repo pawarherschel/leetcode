11586113ac7571f012671334aa99a7823d3f0470
pawarherschel <pawarherschel@gmail.com>
2024-01-22 22:02:52 +0530

    645

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod zaphkiel;
 use cargo_leet::*;
 use std::cell::RefCell;
 use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
-use std::ops::BitAndAssign;
+use std::ops::Not;
 use std::rc::Rc;
 use std::sync::RwLock;
 
@@ -537,57 +537,88 @@ pub fn min_falling_path_sum_931(matrix: Vec<Vec<i32>>) -> i32 {
         .unwrap()
 }
 
+pub fn find_error_nums_645(nums: Vec<i32>) -> Vec<i32> {
+    if nums.len() == 2 {
+        return match nums.as_slice() {
+            &[1, 1] => vec![1, 2],
+            &[2, 2] => vec![2, 1],
+            _ => unreachable!(),
+        };
+    }
+
+    let mut set = BTreeSet::new();
+    let mut dup = None;
+    let mut mis = None;
+
+    nums.into_iter().for_each(|n| {
+        set.insert(n).not().then(|| dup = Some(n));
+    });
+
+    (1..set.last().unwrap() + 2)
+        .any(|check_me| set.insert(check_me).then(|| mis = Some(check_me)).is_some());
+
+    vec![dup.unwrap(), mis.unwrap()]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     generate_tests! {
-         test_217_contains_duplicate_1, contains_duplicate_217, vec![1,2,3,1], true;
-         test_217_contains_duplicate_2, contains_duplicate_217, vec![1,2,3,4], false;
-         test_217_contains_duplicate_3, contains_duplicate_217, vec![1,1,1,3,3,4,3,2,4,2], true;
+     test_217_contains_duplicate_1, contains_duplicate_217, vec![1,2,3,1], true;
+     test_217_contains_duplicate_2, contains_duplicate_217, vec![1,2,3,4], false;
+     test_217_contains_duplicate_3, contains_duplicate_217, vec![1,1,1,3,3,4,3,2,4,2], true;
 
-         test_242_valid_anagram_1, valid_anagram_242, ("anagram".into(), "nagaram".into()), true;
-         test_242_valid_anagram_2, valid_anagram_242, ("rat".into(), "car".into()), false;
+     test_242_valid_anagram_1, valid_anagram_242, ("anagram".into(), "nagaram".into()), true;
+     test_242_valid_anagram_2, valid_anagram_242, ("rat".into(), "car".into()), false;
 
-         test_1_two_sum_1, two_sum_1, (vec![2,7,11,15], 9), vec![0, 1];
-         test_1_two_sum_2, two_sum_1, (vec![3,2,4], 6), vec![1,2];
-         test_1_two_sum_3, two_sum_1, (vec![3,3], 6), vec![0,1];
+     test_1_two_sum_1, two_sum_1, (vec![2,7,11,15], 9), vec![0, 1];
+     test_1_two_sum_2, two_sum_1, (vec![3,2,4], 6), vec![1,2];
+     test_1_two_sum_3, two_sum_1, (vec![3,3], 6), vec![0,1];
 
-         /* test_49_group_anagrams_1, */
+     /* test_49_group_anagrams_1, */
 
-         test_hr_simple_array_sum_1, hr_simple_array_sum, &[1, 2, 3, 4, 10, 11], 31;
-         test_hr_compare_the_triplets, hr_compare_the_triplets, (&[5, 6, 7], &[3, 6, 10]), vec![1, 1];
-         test_hr_a_very_big_sum, a_very_big_sum, &[1000000001, 1000000002, 1000000003, 1000000004, 1000000005], 5000000015;
-         test_hr_diagonal_difference, diagonal_difference, &[vec![11, 2, 4], vec![4, 5, 6], vec![10, 8, -12]], 15;
-         test_hr_plus_minus, plus_minus, &[-4, 3, -9, 0, 4, 1], true;
+     test_hr_simple_array_sum_1, hr_simple_array_sum, &[1, 2, 3, 4, 10, 11], 31;
+     test_hr_compare_the_triplets, hr_compare_the_triplets, (&[5, 6, 7], &[3, 6, 10]), vec![1, 1];
+     test_hr_a_very_big_sum, a_very_big_sum, &[1000000001, 1000000002, 1000000003, 1000000004, 1000000005], 5000000015;
+     test_hr_diagonal_difference, diagonal_difference, &[vec![11, 2, 4], vec![4, 5, 6], vec![10, 8, -12]], 15;
+     test_hr_plus_minus, plus_minus, &[-4, 3, -9, 0, 4, 1], true;
 
-         test_kattis_wheresmyinternet_1, wheresmyinternet, "2 1\n2 1".to_string(), "Connected".to_string();
-         test_kattis_wheresmyinternet_2, wheresmyinternet, "6 4\n1 2\n2 3\n3 4\n5 6".to_string(), "5\n6".to_string();
-         test_kattis_wheresmyinternet_3, wheresmyinternet, "4 3\n2 3\n4 2\n3 4".to_string(), "2\n3\n4".to_string();
+     test_kattis_wheresmyinternet_1, wheresmyinternet, "2 1\n2 1".to_string(), "Connected".to_string();
+     test_kattis_wheresmyinternet_2, wheresmyinternet, "6 4\n1 2\n2 3\n3 4\n5 6".to_string(), "5\n6".to_string();
+     test_kattis_wheresmyinternet_3, wheresmyinternet, "4 3\n2 3\n4 2\n3 4".to_string(), "2\n3\n4".to_string();
 
-         // test_2385_amount_of_time_for_binary_tree_to_be_infected_1, amount_of_time_2385, (TreeRoot::from("[1,5,3,null,4,10,6,9,2]").into(), 3), 4;
-         // test_2385_amount_of_time_for_binary_tree_to_be_infected_2, amount_of_time_2385, (TreeRoot::from("[1,2,null,3,null,4,null,5]").into(), 2), 3;
-         // test_2385_amount_of_time_for_binary_tree_to_be_infected_3, amount_of_time_2385, (TreeRoot::from("[1]").into(), 0), 0;
-         // test_2385_amount_of_time_for_binary_tree_to_be_infected_4, amount_of_time_2385, (TreeRoot::from("[1,2,null,3,null,4,null,5]").into(), 4), 3;
-         // test_2385_amount_of_time_for_binary_tree_to_be_infected_5, amount_of_time_2385, (TreeRoot::from("[5,2,3,4,null,null,null,1]").into(), 4), 3;
-         // test_2385_amount_of_time_for_binary_tree_to_be_infected_6, amount_of_time_2385, (TreeRoot::from("[1,null,2,3,4,null,5]").into(), 4), 3;
+     // test_2385_amount_of_time_for_binary_tree_to_be_infected_1, amount_of_time_2385, (TreeRoot::from("[1,5,3,null,4,10,6,9,2]").into(), 3), 4;
+     // test_2385_amount_of_time_for_binary_tree_to_be_infected_2, amount_of_time_2385, (TreeRoot::from("[1,2,null,3,null,4,null,5]").into(), 2), 3;
+     // test_2385_amount_of_time_for_binary_tree_to_be_infected_3, amount_of_time_2385, (TreeRoot::from("[1]").into(), 0), 0;
+     // test_2385_amount_of_time_for_binary_tree_to_be_infected_4, amount_of_time_2385, (TreeRoot::from("[1,2,null,3,null,4,null,5]").into(), 4), 3;
+     // test_2385_amount_of_time_for_binary_tree_to_be_infected_5, amount_of_time_2385, (TreeRoot::from("[5,2,3,4,null,null,null,1]").into(), 4), 3;
+     // test_2385_amount_of_time_for_binary_tree_to_be_infected_6, amount_of_time_2385, (TreeRoot::from("[1,null,2,3,4,null,5]").into(), 4), 3;
 
-         test_1026_maximum_difference_between_node_and_ancestor_1, max_ancestor_diff_1026, TreeRoot::from("[8,3,10,1,6,null,14,null,null,4,7,13]").into(), 7;
+     test_1026_maximum_difference_between_node_and_ancestor_1, max_ancestor_diff_1026, TreeRoot::from("[8,3,10,1,6,null,14,null,null,4,7,13]").into(), 7;
 
-         test_1704_determine_if_string_halves_are_alike_1, halves_are_alike_1704, "book".into(), true;
+     test_1704_determine_if_string_halves_are_alike_1, halves_are_alike_1704, "book".into(), true;
 
-         test_2225_find_players_with_zero_or_one_losses_1, find_winners_2225, vec![vec![1,3],vec![2,3],vec![3,6],vec![5,6],vec![5,7],vec![4,5],vec![4,8],vec![4,9],vec![10,4],vec![10,9]], vec![vec![1,2,10],vec![4,5,7,8]];
+     test_2225_find_players_with_zero_or_one_losses_1, find_winners_2225, vec![vec![1,3],vec![2,3],vec![3,6],vec![5,6],vec![5,7],vec![4,5],vec![4,8],vec![4,9],vec![10,4],vec![10,9]], vec![vec![1,2,10],vec![4,5,7,8]];
 
-         test_1207_unique_number_of_occurrences_1, unique_occurrences_1207,  vec![1,2,2,1,1,3], true;
+     test_1207_unique_number_of_occurrences_1, unique_occurrences_1207,  vec![1,2,2,1,1,3], true;
 
-         test_70_climbing_stairs_1, climb_stairs_70, 2, 2;
-         test_70_climbing_stairs_2, climb_stairs_70, 3, 3;
-         test_70_climbing_stairs_3, climb_stairs_70, 44, 1134903170;
+     test_70_climbing_stairs_1, climb_stairs_70, 2, 2;
+     test_70_climbing_stairs_2, climb_stairs_70, 3, 3;
+     test_70_climbing_stairs_3, climb_stairs_70, 44, 1134903170;
 
-         test_931_minimum_dalling_path_sum_1, min_falling_path_sum_931, vec![vec![2,1,3],vec![6,5,4],vec![7,8,9]], 13;
-         test_931_minimum_dalling_path_sum_2, min_falling_path_sum_931, vec![vec![-19,57],vec![-40,-5]], -59;
-         test_931_minimum_dalling_path_sum_3, min_falling_path_sum_931,
-            vec![vec![-19,-1,-96,48,-94,36,16,55,-42,37,-59,6,-32,96,95,-58,13,-34,94,85],vec![17,44,36,-29,84,80,-34,50,-99,64,13,91,-27,25,-36,57,20,98,-100,-72],vec![-92,-75,86,90,-4,90,64,56,50,-63,10,-15,90,-66,-66,32,-69,-78,1,60],vec![21,51,-47,-43,-14,99,44,90,8,11,99,-62,57,59,69,50,-69,32,85,13],vec![-28,90,12,-18,23,61,-55,-97,6,89,36,26,26,-1,46,-50,79,-45,89,86],vec![-85,-10,49,-10,2,62,41,92,-67,85,86,27,89,-50,77,55,22,-82,-94,-98],vec![-50,53,-23,55,25,-22,76,-93,-7,66,-75,42,-35,-96,-5,4,-92,13,-31,-100],vec![-62,-78,8,-92,86,69,90,-37,81,97,53,-45,34,19,-19,-39,-88,-75,-74,-4],vec![29,53,-91,65,-92,11,49,26,90,-31,17,-84,12,63,-60,-48,40,-49,-48,88],vec![100,-69,80,11,-93,17,28,-94,52,64,-86,30,-9,-53,-8,-68,-33,31,-5,11],vec![9,64,-31,63,-84,-15,-30,-10,67,2,98,73,-77,-37,-96,47,-97,78,-62,-17],vec![-88,-38,-22,-90,54,42,-29,67,-85,-90,-29,81,52,35,13,61,-18,-94,61,-62],vec![-23,-29,-76,-30,-65,23,31,-98,-9,11,75,-1,-84,-90,73,58,72,-48,30,-81],vec![66,-33,91,-6,-94,82,25,-43,-93,-25,-69,10,-71,-65,85,28,-52,76,25,90],vec![-3,78,36,-92,-52,-44,-66,-53,-55,76,-7,76,-73,13,-98,86,-99,-22,61,100],vec![-97,65,2,-93,56,-78,22,56,35,-24,-95,-13,83,-34,-51,-73,2,7,-86,-19],vec![32,94,-14,-13,-6,-55,-21,29,-21,16,67,100,77,-26,-96,22,-5,-53,-92,-36],vec![60,93,-79,76,-91,43,-95,-16,74,-21,85,43,21,-68,-32,-18,18,100,-43,1],vec![87,-31,26,53,26,51,-61,92,-65,17,-41,27,-42,-14,37,-46,46,-31,-74,23],vec![-67,-14,-20,-85,42,36,56,9,11,-66,-59,-55,5,64,-29,77,47,44,-33,-77]],
-            -1428;
+     test_931_minimum_dalling_path_sum_1, min_falling_path_sum_931, vec![vec![2,1,3],vec![6,5,4],vec![7,8,9]], 13;
+     test_931_minimum_dalling_path_sum_2, min_falling_path_sum_931, vec![vec![-19,57],vec![-40,-5]], -59;
+     test_931_minimum_dalling_path_sum_3, min_falling_path_sum_931,
+        vec![vec![-19,-1,-96,48,-94,36,16,55,-42,37,-59,6,-32,96,95,-58,13,-34,94,85],vec![17,44,36,-29,84,80,-34,50,-99,64,13,91,-27,25,-36,57,20,98,-100,-72],vec![-92,-75,86,90,-4,90,64,56,50,-63,10,-15,90,-66,-66,32,-69,-78,1,60],vec![21,51,-47,-43,-14,99,44,90,8,11,99,-62,57,59,69,50,-69,32,85,13],vec![-28,90,12,-18,23,61,-55,-97,6,89,36,26,26,-1,46,-50,79,-45,89,86],vec![-85,-10,49,-10,2,62,41,92,-67,85,86,27,89,-50,77,55,22,-82,-94,-98],vec![-50,53,-23,55,25,-22,76,-93,-7,66,-75,42,-35,-96,-5,4,-92,13,-31,-100],vec![-62,-78,8,-92,86,69,90,-37,81,97,53,-45,34,19,-19,-39,-88,-75,-74,-4],vec![29,53,-91,65,-92,11,49,26,90,-31,17,-84,12,63,-60,-48,40,-49,-48,88],vec![100,-69,80,11,-93,17,28,-94,52,64,-86,30,-9,-53,-8,-68,-33,31,-5,11],vec![9,64,-31,63,-84,-15,-30,-10,67,2,98,73,-77,-37,-96,47,-97,78,-62,-17],vec![-88,-38,-22,-90,54,42,-29,67,-85,-90,-29,81,52,35,13,61,-18,-94,61,-62],vec![-23,-29,-76,-30,-65,23,31,-98,-9,11,75,-1,-84,-90,73,58,72,-48,30,-81],vec![66,-33,91,-6,-94,82,25,-43,-93,-25,-69,10,-71,-65,85,28,-52,76,25,90],vec![-3,78,36,-92,-52,-44,-66,-53,-55,76,-7,76,-73,13,-98,86,-99,-22,61,100],vec![-97,65,2,-93,56,-78,22,56,35,-24,-95,-13,83,-34,-51,-73,2,7,-86,-19],vec![32,94,-14,-13,-6,-55,-21,29,-21,16,67,100,77,-26,-96,22,-5,-53,-92,-36],vec![60,93,-79,76,-91,43,-95,-16,74,-21,85,43,21,-68,-32,-18,18,100,-43,1],vec![87,-31,26,53,26,51,-61,92,-65,17,-41,27,-42,-14,37,-46,46,-31,-74,23],vec![-67,-14,-20,-85,42,36,56,9,11,-66,-59,-55,5,64,-29,77,47,44,-33,-77]],
+        -1428;
+
+    test_645_set_mismatch_1, find_error_nums_645, vec![1,2,2,4], vec![2,3];
+    test_645_set_mismatch_2, find_error_nums_645, vec![3,2,2], vec![2,1];
+    test_645_set_mismatch_3, find_error_nums_645, vec![1,1], vec![1,2];
+    test_645_set_mismatch_4, find_error_nums_645, vec![2,2], vec![2,1];
+    test_645_set_mismatch_5, find_error_nums_645, vec![1,3,3], vec![3,2];
+    test_645_set_mismatch_6, find_error_nums_645, vec![3,2,3,4,6,5], vec![3,1];
+    test_645_set_mismatch_7, find_error_nums_645, vec![2, 3, 2], vec![2,1];
     }
 }
